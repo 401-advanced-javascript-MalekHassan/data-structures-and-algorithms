d4ae15a402bc254d5fc66b3c4d348fa134b953d6
MalekHassan <malekdalalha1995@gmail.com>
2020-10-23 22:03:16 +0300

    doing the lint

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,14 +1,14 @@
 'use strict';
 
-let arrayA = [1,2,3,4,5]
-function reverse (array){
-    let reversedArray = [];
-    
-    for (let index = array.length - 1 ; index>= 0; index--) {
-        reversedArray.push(array[index])
-    }
-    return reversedArray;
+let arrayA = [1, 2, 3, 4, 5];
+function reverse(array) {
+  let reversedArray = [];
+
+  for (let index = array.length - 1; index >= 0; index--) {
+    reversedArray.push(array[index]);
+  }
+  return reversedArray;
 }
-reverse(arrayA)
+reverse(arrayA);
 
 console.log(reverse(arrayA));
